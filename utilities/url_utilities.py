242e106b3f822178f16aaf88b58208a49c5b7c55
Mohit Sharma <ms24546@gmail.com>
2017-12-10 13:59:14 +0530

    FIxed Formatting Issues

--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,11 +1,13 @@
 def load_urls_from_files(file_path: str):
-    #TODO: add the file needed to read text files with urls in it
+    # TODO: add the file needed to read text files with urls in it
     pass
+
 
 def load_page(url: str):
-    #TODO: add the code that reads the url
+    # TODO: add the code that reads the url
     pass
 
+
 def scrape_page(page_contents: str):
-    #TODO: analyze the text
+    # TODO: analyze the text
     pass
