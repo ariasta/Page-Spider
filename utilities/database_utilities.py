057f88e5d2520fc2ca4a02b3751facea11aa7882
Mohit Sharma <ms24546@gmail.com>
2017-12-10 13:46:43 +0530

    Initial Commit

---
+++ utilities/database_utilities.py
@@ -0,0 +1,8 @@
+def create_database(database_path: str):
+    #TODO: generate the sqlite database
+    pass
+
+
+def save_stuff_to_database(database_path: str, words_list: list):
+    #TODO: this saves stuff to our database
+    pass
---
+++ utilities/url_utilities.py
@@ -0,0 +1,11 @@
+def load_urls_from_files(file_path: str):
+    #TODO: add the file needed to read text files with urls in it
+    pass
+
+def load_page(url: str):
+    #TODO: add the code that reads the url
+    pass
+
+def scrape_page(page_contents: str):
+    #TODO: analyze the text
+    pass
